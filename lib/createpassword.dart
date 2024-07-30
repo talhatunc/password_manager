fcc260343d2fd0da3cc55e6ae8115e847880a4c8
Muhammed Talha TUNÇ <142929908+talhatunc@users.noreply.github.com>
2024-07-30 09:34:59 +0300

    feat: Add password generator screen with customizable options and clipboard copy functionality
    - Implement password generation logic with options for numbers, uppercases, symbols, and length
    - Add UI components for setting password criteria and displaying generated password

---
+++ lib/createpassword.dart
@@ -0,0 +1,207 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class PasswordGeneratorScreen extends StatefulWidget {
+  @override
+  _PasswordGeneratorScreenState createState() => _PasswordGeneratorScreenState();
+}
+
+class _PasswordGeneratorScreenState extends State<PasswordGeneratorScreen> {
+  int _numbers = 0;
+  int _uppercases = 0;
+  int _symbols = 0;
+  int _characters = 9;
+  String _generatedPassword = "ztsqmxnlc";
+
+  void _generatePassword() {
+    const String numbers = '0123456789';
+    const String uppercases = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const String symbols = '!@#\$%^&*()_+[]{}|;:,.<>?';
+    const String lowercases = 'abcdefghijklmnopqrstuvwxyz';
+
+    String allChars = lowercases;
+    if (_numbers > 0) allChars += numbers;
+    if (_uppercases > 0) allChars += uppercases;
+    if (_symbols > 0) allChars += symbols;
+
+    Random random = Random();
+
+    String newPassword = '';
+    for (int i = 0; i < _characters; i++) {
+      newPassword += allChars[random.nextInt(allChars.length)];
+    }
+
+    setState(() {
+      _generatedPassword = newPassword;
+    });
+  }
+
+  void _copyToClipboard() async {
+    await Clipboard.setData(ClipboardData(text: _generatedPassword)).then((result) {
+      final snackBar = SnackBar(content: Text('Password copied to clipboard'));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Password Generator'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "Create a Solid Password",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _buildCounter("Numbers", _numbers, (val) {
+                    setState(() {
+                      _numbers = val;
+                    });
+                  }),
+                  _buildCounter("Uppercases", _uppercases, (val) {
+                    setState(() {
+                      _uppercases = val;
+                    });
+                  }),
+                  _buildCounter("Symbols", _symbols, (val) {
+                    setState(() {
+                      _symbols = val;
+                    });
+                  }),
+                ],
+              ),
+              SizedBox(height: 20),
+              Expanded(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "CHARACTERS",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Text(
+                        "$_characters",
+                        style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                      ),
+                      Slider(
+                        activeColor: Colors.blue,
+                        value: _characters.toDouble(),
+                        min: 1,
+                        max: 20,
+                        divisions: 19,
+                        label: _characters.toString(),
+                        onChanged: (double value) {
+                          setState(() {
+                            _characters = value.toInt();
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Text(
+                "GENERATED PASSWORD",
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    _generatedPassword,
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.copy),
+                    onPressed: _copyToClipboard,
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.blue,
+                    ),
+                    onPressed: _generatePassword,
+                    child: Row(
+                      children: [
+                        Icon(Icons.refresh),
+                        SizedBox(width: 5),
+                        Text("Generate"),
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.blue,
+                    ),
+                    onPressed: () {
+
+                    },
+                    child: Row(
+                      children: [
+                        Icon(Icons.check),
+                        SizedBox(width: 5),
+                        Text("Confirm"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCounter(String label, int value, Function(int) onChanged) {
+    return Column(
+      children: [
+        Text(label, style: TextStyle(fontSize: 16)),
+        Row(
+          children: [
+            IconButton(
+              icon: Icon(Icons.arrow_downward),
+              onPressed: () {
+                if (value > 0) {
+                  onChanged(value - 1);
+                }
+              },
+            ),
+            Text("$value", style: TextStyle(fontSize: 20)),
+            IconButton(
+              icon: Icon(Icons.arrow_upward),
+              onPressed: () {
+                onChanged(value + 1);
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'createpassword.dart';
 void main() {
   runApp(PasswordManagerApp());
 }
@@ -24,22 +25,32 @@ class PasswordManagerScreen extends StatefulWidget {
 
 class _PasswordManagerScreenState extends State<PasswordManagerScreen> {
   int _selectedIndex = 0;
+  PageController _pageController = PageController();
 
-  static final List<Widget> _widgetOptions = <Widget>[
-    PasswordListScreen(),
-    Center(child: Text('Social', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
-    Center(child: Text('Entertainment', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
-    Center(child: Text('Education', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
+  List<PasswordItem> allPasswords = [
+    PasswordItem(icon: FaIcon(FontAwesomeIcons.instagram), title: 'Instagram', username: 'talhatuncc__', tab: 'S'),
+    PasswordItem(icon: FaIcon(FontAwesomeIcons.xTwitter), title: 'X', username: 'talhatunc', tab: 'S'),
+    PasswordItem(icon: FaIcon(FontAwesomeIcons.linkedinIn), title: 'LinkedIn', username: 'talhatunc', tab: 'S'),
+    PasswordItem(icon: FaIcon(FontAwesomeIcons.amazon), title: 'Amazon', username: 'talhatunc', tab: 'S'),
+    PasswordItem(icon: FaIcon(FontAwesomeIcons.n), title: 'Netflix', username: 'talhatunc', tab: 'M'),
+    PasswordItem(icon: FaIcon(FontAwesomeIcons.u), title: 'Udemy', username: 'talhatunc', tab: 'E'),
   ];
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+  List<PasswordItem> get socialPasswords => allPasswords.where((item) => item.tab == 'S').toList();
+
+  List<PasswordItem> get entertainmentPasswords => allPasswords.where((item) => item.tab == 'M').toList();
+
+  List<PasswordItem> get educationPasswords => allPasswords.where((item) => item.tab == 'E').toList();
 
   @override
   Widget build(BuildContext context) {
+    final List<Widget> widgetOptions = [
+      PasswordListScreen(passwords: allPasswords),
+      PasswordListScreen(passwords: socialPasswords),
+      PasswordListScreen(passwords: entertainmentPasswords),
+      PasswordListScreen(passwords: educationPasswords),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Keep your Passwords safe'),
@@ -47,12 +58,19 @@ class _PasswordManagerScreenState extends State<PasswordManagerScreen> {
           IconButton(
             icon: Icon(Icons.add),
             onPressed: () {
-
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PasswordGeneratorScreen()),
+              );
             },
           ),
         ],
       ),
-      body: _widgetOptions.elementAt(_selectedIndex),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: _onPageChanged,
+        children: widgetOptions,
+      ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -74,24 +92,40 @@ class _PasswordManagerScreenState extends State<PasswordManagerScreen> {
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.blue,
+        unselectedItemColor: Color(0xFFDCDCDC), //gainsboro
         onTap: _onItemTapped,
       ),
     );
   }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    _pageController.jumpToPage(index);
+  }
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 }
 
 class PasswordListScreen extends StatelessWidget {
+  final List<PasswordItem> passwords;
+
+  PasswordListScreen({required this.passwords});
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: ListView(
         children: <Widget>[
-          Text('0 Passwords', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+          Text('${passwords.length} Passwords', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
           SizedBox(height: 20),
-          PasswordItem(icon: FaIcon(FontAwesomeIcons.instagram), title: 'Instagram', username: 'talhatuncc__'),
-          PasswordItem(icon: FaIcon(FontAwesomeIcons.twitter), title: 'X', username: 'talhatunc'),
-          PasswordItem(icon: FaIcon(FontAwesomeIcons.linkedinIn),title: 'LinkedIn', username: 'talhatunc',),
+          ...passwords,
         ],
       ),
     );
@@ -102,14 +136,16 @@ class PasswordItem extends StatelessWidget {
   final FaIcon icon;
   final String title;
   final String username;
+  final String tab;
 
-  PasswordItem({required this.icon, required this.title, required this.username});
+  PasswordItem({required this.icon, required this.title, required this.username, required this.tab});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.symmetric(vertical: 10),
       child: ListTile(
+        contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
         leading: icon,
         title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
         subtitle: Text(username),
